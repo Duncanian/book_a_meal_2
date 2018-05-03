51c78f77fdbd449328e0662cb5c4f87bd5cefb4f
Duncanian <ianduncan08@gmail.com>
2018-05-03 15:47:19 +0300

    Chore : Import default app for develop

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from base import create_app
+from base import app
 from config import config
-
-app = create_app(config['development'])
 
 if __name__ == "__main__":
 	app.run(debug=True, host='127.0.0.1', port=5000)
