6ca380c1a56e6e7cf64fcd151951090741d443de
Duncanian <ianduncan08@gmail.com>
2018-05-04 03:54:27 +0300

    Chore : Change the api endpoints from v1 to v2

--- base.py
+++ base.py
@@ -15,14 +15,14 @@ def create_app(config_filename):
     db.init_app(app)
 
     api.add_resource(Hello, '/')
-    api.add_resource(AuthCreate, '/api/v1/auth/signup', '/api/v1/users')
-    api.add_resource(AuthLogin, '/api/v1/auth/login')
-    api.add_resource(MealMan, '/api/v1/meals/', '/api/v1/meals/',
-    	                '/api/v1/meals/<string:meal_id>', '/api/v1/meals/<string:meal_id>')
-    api.add_resource(Menu, '/api/v1/menu/')
-    api.add_resource(OrdersAll, '/api/v1/orders')
-    api.add_resource(MenuOrders, '/api/v1/menu/', '/api/v1/orders',
-    	                '/api/v1/orders/<string:order_id>', '/api/v1/orders/<string:order_id>')
+    api.add_resource(AuthCreate, '/api/v2/auth/signup', '/api/v2/users')
+    api.add_resource(AuthLogin, '/api/v2/auth/login')
+    api.add_resource(MealMan, '/api/v2/meals/', '/api/v2/meals/',
+    	                '/api/v2/meals/<string:meal_id>', '/api/v2/meals/<string:meal_id>')
+    api.add_resource(Menu, '/api/v2/menu/')
+    api.add_resource(OrdersAll, '/api/v2/orders')
+    api.add_resource(MenuOrders, '/api/v2/menu/', '/api/v2/orders',
+    	                '/api/v2/orders/<string:order_id>', '/api/v2/orders/<string:order_id>')
 
     return app
 
