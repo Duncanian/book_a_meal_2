1386bd40d86c62994be862f20096d39f2cdc0ba1
Duncanian <ianduncan08@gmail.com>
2018-05-05 23:24:05 +0300

    Chore : Add extra '\' to endpoints

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from app import Hello
 from views.user_res import AuthCreate, AuthLogin, MenuOrders
-from views.cater_res import MealMan, Menu, OrdersAll
+from views.cater_res import MealMan, Menus, OrdersAll
 from config import config
 
 def create_app(config_filename):
@@ -15,14 +15,21 @@ def create_app(config_filename):
     db.init_app(app)
 
     api.add_resource(Hello, '/')
-    api.add_resource(AuthCreate, '/api/v2/auth/signup', '/api/v2/users')
-    api.add_resource(AuthLogin, '/api/v2/auth/login')
-    api.add_resource(MealMan, '/api/v2/meals/', '/api/v2/meals/',
-    	                '/api/v2/meals/<string:meal_id>', '/api/v2/meals/<string:meal_id>')
-    api.add_resource(Menu, '/api/v2/menu/')
-    api.add_resource(OrdersAll, '/api/v2/orders')
-    api.add_resource(MenuOrders, '/api/v2/menu/', '/api/v2/orders',
-    	                '/api/v2/orders/<string:order_id>', '/api/v2/orders/<string:order_id>')
+
+    api.add_resource(AuthCreate, '/api/v2/auth/signup', '/api/v2/auth/signup/', '/api/v2/users', '/api/v2/users/')
+
+    api.add_resource(AuthLogin, '/api/v2/auth/login', '/api/v2/auth/login/')
+
+    api.add_resource(MealMan, '/api/v2/meals/', '/api/v2/meals', '/api/v2/meals/', '/api/v2/meals', '/api/v2/meals/<int:meal_id>', 
+                        '/api/v2/meals/<int:meal_id>/', '/api/v2/meals/<int:meal_id>', '/api/v2/meals/<int:meal_id>/')
+
+    api.add_resource(Menus, '/api/v2/menu/', '/api/v2/menu')
+
+    api.add_resource(OrdersAll, '/api/v2/orders', '/api/v2/orders/')
+
+    api.add_resource(MenuOrders, '/api/v2/menu/', '/api/v2/menu', '/api/v2/orders', '/api/v2/orders/', 
+                                 '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/', 
+                                 '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/')
 
     return app
 
