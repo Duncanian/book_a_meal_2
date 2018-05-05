3baa38fd027f565b2935628d18c280ab4f184f1d
Duncanian <ianduncan08@gmail.com>
2018-05-05 23:36:00 +0300

    Chore : Implement possible edge cases

--- views/cater_res.py
+++ views/cater_res.py
@@ -1,28 +1,38 @@
-from flask import jsonify, request, make_response
+from flask import request
 import uuid
+from os import getenv
+import jwt
 from models.models import Meals, Orders, Menu, db
 from flask_restful import Resource
 from auth.token import token_required, admin_only
+import datetime
 
 class MealMan(Resource):
 	"""docstring for Meal_man"""
 	@admin_only
 	def post(self, active_user):
 		post_data = request.get_json(force=True)
-		if post_data['meal_name'] == '' or post_data['meal_price'] == '':
-			return jsonify({'message' : 'Please enter all the details'})
+		meal = Meals.query.filter_by(meal_name=post_data['meal_name']).first()
 
-		if not isinstance(post_data['meal_name'], str):
-			return jsonify({'message' : 'Please enter a string value for meal'})
+		if not post_data['meal_name'] or not post_data['meal_price']:
+			return {'message' : 'Please enter all the details'}, 200
 
 		if not isinstance(post_data['meal_price'], int):
-			return jsonify({'message' : 'Price should be a number'})
+			return {'message' : 'Price should be a number'}, 200
 
-		new_meal = Meals(meal_id = str(uuid.uuid4()), meal_name = post_data['meal_name'], meal_price = post_data['meal_price'],
-			meal_category = post_data['meal_category'], meal_day = post_data['meal_day'])
+		if not isinstance(post_data['meal_name'], str):
+			return {'message' : 'Please enter a string value for meal'}, 200
+
+		if len(post_data['meal_name'].strip()) < len(post_data['meal_name']):
+			return {'message' : 'Meal name should not have spaces!'}, 200
+
+		if meal:
+			return {"message" : "The meal already exists"}, 200
+
+		new_meal = Meals(meal_name = post_data['meal_name'], meal_price = post_data['meal_price'])
 		db.session.add(new_meal)
 		db.session.commit()
-		return jsonify({'message' : 'New meal added!'})
+		return {'message' : 'New meal added!'}, 200
 
 	@admin_only
 	def get(self, active_user):
@@ -30,11 +40,9 @@ class MealMan(Resource):
 		output = []
 		for meal in meals:
 			meal_data = {}
-			meal_data['meal_id'] = meal.meal_id
+			meal_data['meal_id'] = meal.id
 			meal_data['meal_name'] = meal.meal_name
 			meal_data['meal_price'] = meal.meal_price
-			meal_data['meal_category'] = meal.meal_category
-			meal_data['meal_day'] = meal.meal_day
 			output.append(meal_data)
 
 		return {"status": "success", "data": output}, 200
@@ -42,64 +50,102 @@ class MealMan(Resource):
 	@admin_only
 	def put(self, active_user, meal_id):
 		post_data = request.get_json(force=True)
-		meal = Meals.query.filter_by(meal_id=meal_id).first()
+		meal = Meals.query.filter_by(id=meal_id).first()
+
+		if not post_data['meal_name'] or not post_data['meal_price']:
+			return {'message' : 'Please enter all the details'}, 200
+
+		if not isinstance(post_data['meal_name'], str):
+			return {'message' : 'Please enter a string value for meal'}, 200
+
+		if not isinstance(post_data['meal_price'], int):
+			return {'message' : 'Please enter a number value for price'}, 200
+
+		if len(post_data['meal_name'].strip()) < len(post_data['meal_name']):
+			return {'message' : 'Meal name should not have spaces!'}, 200
 
 		if not meal:
-			return jsonify({"message" : "The meal was not found"})
-
-		if request.json['meal_name'] == '' or request.json['meal_price'] == '':
-			return jsonify({'message' : 'Please enter all the details'})
-
-		if not isinstance(request.json['meal_name'], str):
-			return jsonify({'message' : 'Please enter a string value for meal'})
+			return {"message" : "The meal was not found"}, 200
 
 		meal.meal_name = post_data['meal_name']
 		meal.meal_price = post_data['meal_price']
-		meal.meal_category = post_data['meal_category']
-		meal.meal_day = post_data['meal_day']
 		db.session.commit()
 
 		return {"status": "success", "data": 'Meal modified!'}, 200
 
 	@admin_only
 	def delete(self, active_user, meal_id):
-		meal = Meals.query.filter_by(meal_id=meal_id).first()
+		meal = Meals.query.filter_by(id=meal_id).first()
 
 		if not meal:
-			return jsonify({"message" : "The meal was not found"})
+			return {"message" : "The meal was not found"}, 200
 		db.session.delete(meal)
 		db.session.commit()
-		return jsonify({"message" : "The meal has been deleted"})
+		return {"message" : "The meal has been deleted"}, 200
 
-class Menu(Resource):
+class Menus(Resource):
 	"""docstring for Menu"""
 	@admin_only
 	def post(self, active_user):
 		post_data = request.get_json(force=True)
-		meal = Meals.query.filter_by(meal_name=post_data['menu_name']).first()
 
-		if not meal:
-			return jsonify({"message" : "The meal was not found"})
+		if not post_data['meal_ids']:
+			return {"message":"Please enter an id to add your meals to the menu"}, 200
 
-		new_menu = Menu(menu_id = meal.meal_id, menu_name = post_data['menu_name'], menu_price = post_data['menu_price'], menu_category = post_data['menu_category'], menu_day = post_data['menu_day'])
+		if not isinstance(post_data['meal_ids'], list):
+			return {'message' : 'Please enter list id values for meals'}, 200
+
+		for i in post_data['meal_ids']:
+			if not isinstance(i, int):
+				return {"message":"List values should only be in numbers"}, 200
+
+		if not post_data['menu_name']:
+			return {'message' : 'Please enter the Menu name'}, 200
+
+		if not isinstance(post_data['menu_name'], str):
+			return {'message' : 'Please enter a string value for Menu name'}, 200
+
+		if len(post_data['menu_name'].strip()) < len(post_data['menu_name']):
+			return {'message' : 'Menu name should not have spaces!'}, 200
+
+		meal_ids = post_data['meal_ids']
+		menu_meals = []
+		for i in meal_ids:
+			meal = Meals.query.filter_by(id=i).first()
+			if not meal:
+				return {"message" : "The meal was not found"}, 200
+			menu_meals.append(meal)
+
+		menu = Menu.query.filter_by(menu_name=post_data['menu_name']).first()
+		if menu:
+			return {'message':"Menu already available!"}, 200
+
+		new_menu = Menu(menu_name = post_data['menu_name'])
 		db.session.add(new_menu)
 		db.session.commit()
-		return jsonify({'message' : 'New meal added to the menu!'})
+		new_menu = Menu.query.filter_by(id=new_menu.id).first()
+		new_menu.meals.extend(menu_meals)
+		db.session.add(new_menu)
+		db.session.commit()
+		return {'message' : 'New meal added to the menu!'}, 200
 
 class OrdersAll(Resource):
 	"""docstring for Orders"""
 	@admin_only
 	def get(self, active_user):
-		orders = Orders.query.all()
+		if 'Authorization' in request.headers:
+			token = request.headers['Authorization']
+
+		user_det = jwt.decode(token, getenv('SECRET_KEY'))
+
+		orders = Orders.query.filter_by(order_by=user_det['user_id']).all()
 		output = []
 		for order in orders:
 			order_data = {}
-			order_data['order_id'] = order.order_id
-			order_data['order_name'] = order.order_name
+			order_data['id'] = order.id
+			order_data['order_meal'] = order.order_meal
 			order_data['order_price'] = order.order_price
-			order_data['order_category'] = order.order_category
-			order_data['order_day'] = order.order_day
-			order_data['order_qty'] = order.order_qty
-			order_data['order_user'] = order.order_user
+			order_data['time_ordered'] = str(order.time_ordered)
+			order_data['qty'] = order.qty
 			output.append(order_data)
 		return {"status": "success", "data": output}, 200
--- views/user_res.py
+++ views/user_res.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, make_response
+from flask import request
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 from models.models import User, Orders, Menu, db, Meals
@@ -12,24 +12,31 @@ class AuthCreate(Resource):
     def post(self):
         post_data = request.get_json(force=True)
 
-        if post_data['password'] == '' or post_data['username'] == '':
-            return jsonify({'message' : 'Please enter all the details'})
+        if not post_data['password'] or not post_data['username']:
+            return {'message' : 'Please enter all the details'}, 200
 
         if not isinstance(post_data['password'], str) or not isinstance(post_data['username'], str):
-            return jsonify({'message' : 'Please enter a string value for username and password'})
+            return {'message' : 'Please enter a string value for username and password'}, 200
+
+        if len(post_data['username'].strip()) < len(post_data['username']):
+            return {'message' : 'Username should not have spaces!'}, 200
+
+        if len(post_data['password']) < 5:
+            return {'message' : 'Password should be more than 5 characters'}, 200
+
+        if len(post_data['password']) > 10:
+            return {'message' : 'Password should be less than 10 characters'}, 200
+
+        user = User.query.filter_by(username = post_data['username']).first()
+
+        if user:
+            return {"message":"Sorry, username already taken!"}
 
         hashed_password = generate_password_hash(post_data['password'], method='md5')
-        new_user = User(user_id = str(uuid.uuid4()), username = post_data['username'], 
-            password = hashed_password, admin=False)
+        new_user = User(username = post_data['username'], password = hashed_password, admin=False)
         db.session.add(new_user)
         db.session.commit()
-        return jsonify({'message' : 'New user created!'})
-
-        # hashed_password = generate_password_hash(request.form.get('password'), method='md5')
-        # new_user = User(user_id = int(uuid.uuid4()), username = request.form.get('username'), password = hashed_password, admin=False)
-        # db.session.add(new_user)
-        # db.session.commit()
-        # return jsonify({'message' : 'New user created!'})
+        return {'message' : 'New user created!'}, 200
 
     @admin_only
     def get(self, active_user):
@@ -37,7 +44,7 @@ class AuthCreate(Resource):
     	output = []
     	for user in users:
     		user_data = {}
-    		user_data['user_id'] = user.user_id
+    		user_data['id'] = user.id
     		user_data['username'] = user.username
     		user_data['password'] = user.password
     		output.append(user_data)
@@ -49,35 +56,34 @@ class AuthLogin(Resource):
     def post(self):
         post_data = request.get_json(force=True)
 
-        if post_data['password'] == '' or post_data['username'] == '':
-            return jsonify({'message' : 'Please enter all the details'})
+        if not post_data['password'] or not post_data['username']:
+            return {'message' : 'Please enter all the details'}, 200
 
         if not isinstance(post_data['password'], str) or not isinstance(post_data['username'], str):
-            return jsonify({'message' : 'Please enter a string value for username and password'})
+            return {'message' : 'Please enter a string value for username and password'}, 200
 
         user = User.query.filter_by(username = post_data['username']).first()
 
         if not user:
-            return jsonify({'message' : 'Please sign up then login'})
+            return {'message' : 'Please sign up then login'}, 200
 
         if check_password_hash(user.password, post_data['password']):
-            token = jwt.encode({"user_id" : user.user_id, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes = 30)}, getenv('SECRET_KEY'))
-            return jsonify({"token" : token.decode('UTF-8')})
-        return {"message":"wrong password, please try again"}
+            token = jwt.encode({"user_id" : user.id, "admin":user.admin, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes = 30)}, getenv('SECRET_KEY'))
+            return {"token" : token.decode('UTF-8')}, 200
+        return {"message":"wrong password, please try again"}, 200
         
 
 class MenuOrders(Resource):
     """docstring for MenuOrders"""
     @token_required
     def get(self, active_user):
-        menu = Menu.query.all()
+        #date = datetime.datetime.utcnow().date()
+        menu = Menu.query.filter_by(id=1).first()
         output = []
-        for meal in menu:
+        for meal in menu.meals:
             menu_data = {}
-            menu_data['menu_name'] = meal.menu_name
-            menu_data['menu_price'] = meal.menu_price
-            menu_data['menu_category'] = meal.menu_category
-            menu_data['menu_day'] = meal.menu_day
+            menu_data['menu_name'] = meal.meal_name
+            menu_data['menu_price'] = meal.meal_price
             output.append(menu_data)
 
         return {"status": "success", "data": output}, 200
@@ -85,42 +91,84 @@ class MenuOrders(Resource):
     @token_required
     def post(self, active_user):
         post_data = request.get_json(force=True)
-        meal = Menu.query.filter_by(menu_name=post_data['order_name']).first()
 
-        if not meal:
-            return jsonify({"message" : "The meal was not found in menu"})
+        if not post_data['meal_ids']:
+            return {"message":"Please enter an id to add your meals to the menu"}, 200
 
-        order = Orders.query.filter_by(order_name=meal.menu_name).first()
+        if not isinstance(post_data['meal_ids'], list):
+            return {'message' : 'Please enter list id values for meals'}, 200
 
-        if order:
-            return jsonify({"message" : "The order exist!"})
+        for i in post_data['meal_ids']:
+            if not isinstance(i, int):
+                return {"message":"List values should only be in numbers"}, 200
 
-        new_order = Orders(order_id = meal.menu_id, order_name = post_data['order_name'], order_price = post_data['order_price'],
-            order_category = post_data['order_category'], order_day = post_data['order_day'], order_qty=1, order_user = post_data['order_user'])
-        #Add name from jwt
-        db.session.add(new_order)
-        db.session.commit()
-        return jsonify({'message' : 'Your order has been placed!'})
+        meal_ids = post_data['meal_ids']
+
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+        user_det = jwt.decode(token, getenv('SECRET_KEY'))
+
+        menu = Menu.query.filter_by(id=1).first()
+        meal_list = []
+        output = []
+        for meal in menu.meals:
+            yes = [x for x in meal_ids if x == meal.id]
+            meal_list.append(yes[0])
+
+            menu_data = {}
+            menu_data['id'] = meal.id
+            menu_data['meal_name'] = meal.meal_name
+            menu_data['meal_price'] = meal.meal_price
+            output.append(menu_data)
+
+        incorrect = [meal_id for meal_id in meal_ids if meal_id not in meal_list]
+        if len(incorrect) > 0:
+            return {"message" : 'Please enter food that is in the menu'}, 200
+
+        for item in meal_list:
+            orders = [order for order in output if order["id"] == item]
+            one_order = orders[0]
+
+            order_ava = Orders.query.filter_by(order_meal=one_order['meal_name']).first()
+            if order_ava:
+                return {'message' : 'Sorry, the meal is already in your order'}, 200
+
+            new_order = Orders(order_meal = one_order['meal_name'], 
+                               order_price = one_order['meal_price'], qty=1, order_by=user_det['user_id'])
+            db.session.add(new_order)
+            db.session.commit()
+        
+        return {'message' : 'Your order was successfully created!'}, 200
 
     @token_required
     def put(self, active_user, order_id):
+        #change qty then multiply it to get the price
+
         post_data = request.get_json(force=True)
-        order = Orders.query.filter_by(order_id=order_id).first()
+        order = Orders.query.filter_by(id=order_id).first()
 
         if not order:
-            return jsonify({"message" : "The order was not found"})
+            return {"message" : "The order was not found"}, 200
 
-        order.order_qty = post_data['order_qty']
+        if not post_data['qty']:
+            return {'message' : 'Please enter the quantity you want to change to'}, 200
+
+        if not isinstance(post_data['qty'], int):
+            return {'message' : 'Please enter a number value for quantity'}, 200
+
+        order.qty = post_data['qty']
+        total = order.qty * order.order_price
+        order.order_price = total
         db.session.commit()
 
         return {"status": "success", "data": 'Order modified!'}, 200
 
     @token_required
     def delete(self, active_user, order_id):
-        order = Orders.query.filter_by(order_id=order_id).first()
+        order = Orders.query.filter_by(id=order_id).first()
 
         if not order:
-            return jsonify({"message" : "The order was not found"})
+            return {"message" : "The order was not found"}, 200
         db.session.delete(order)
         db.session.commit()
-        return jsonify({"message" : "The order has been removed"})
+        return {"message" : "The order has been removed"}, 200
