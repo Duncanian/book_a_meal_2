e5358931c0aca24444189441154d44879abda630
Duncanian <ianduncan08@gmail.com>
2018-05-06 21:40:29 +0300

    Chore : Change db models in order table

--- models/models.py
+++ models/models.py
@@ -2,48 +2,53 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class User(db.Model):
-	"""docstring for Users"""
-	__tablename__ = "users"
-	id = db.Column(db.Integer, primary_key = True)
-	username = db.Column(db.String(50))
-	password = db.Column(db.String(80))
-	admin = db.Column(db.Boolean)
 
-	def __repr__(self):
-		return '<User {}>'.format(self.id)
+class User(db.Model):
+    """docstring for Users"""
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.id)
+
 
 class Meals(db.Model):
-	"""docstring for Meals"""
-	__tablename__ = "meals"
-	id = db.Column(db.Integer, primary_key = True)
-	meal_name = db.Column(db.String(50))
-	meal_price = db.Column(db.Integer)
-	menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=True)
+    """docstring for Meals"""
+    __tablename__ = "meals"
+    id = db.Column(db.Integer, primary_key=True)
+    meal_name = db.Column(db.String(50))
+    meal_price = db.Column(db.Integer)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=True)
 
-	def __repr__(self):
-		return '<Meals {}>'.format(self.id)
+    def __repr__(self):
+        return '<Meals {}>'.format(self.id)
+
 
 class Menu(db.Model):
-	"""docstring for Meals"""
-	__tablename__ = "menu"
-	id = db.Column(db.Integer, primary_key = True)
-	menu_name = db.Column(db.String(50))
-	menu_date = db.Column(db.String, default=datetime.utcnow().date())
-	meals = db.relationship('Meals', backref='meals', uselist=True, lazy=True)
+    """docstring for Meals"""
+    __tablename__ = "menu"
+    id = db.Column(db.Integer, primary_key=True)
+    menu_name = db.Column(db.String(50))
+    menu_date = db.Column(db.String, default=datetime.utcnow().date())
+    meals = db.relationship('Meals', backref='meals', uselist=True, lazy=True)
 
-	def __repr__(self):
-		return '<Menu {}>'.format(self.id)
+    def __repr__(self):
+        return '<Menu {}>'.format(self.id)
+
 
 class Orders(db.Model):
-	"""docstring for Orders"""
-	__tablename__ = "orders"
-	id = db.Column(db.Integer, primary_key = True)
-	order_meal = db.Column(db.String(50))
-	order_price = db.Column(db.Integer)
-	time_ordered = db.Column(db.String, default=datetime.utcnow().date())
-	qty = db.Column(db.Integer)
-	order_by= db.Column(db.Integer)
+    """docstring for Orders"""
+    __tablename__ = "orders"
+    id = db.Column(db.Integer, primary_key=True)
+    order_meal = db.Column(db.String(50))
+    order_price = db.Column(db.Integer)
+    order_date = db.Column(db.String, default=datetime.utcnow().date())
+    order_time = db.Column(db.String, default=datetime.utcnow().time())
+    qty = db.Column(db.Integer)
+    order_by = db.Column(db.Integer)
 
-	def __repr__(self):
-		return '<Orders {}>'.format(self.id)
+    def __repr__(self):
+        return '<Orders {}>'.format(self.id)
