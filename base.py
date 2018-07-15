34586c76ec12edd8a7b591d0b6cf4b21d7a3f399
Ian Duncan <ianduncan08@gmail.com>
2018-07-15 15:00:17 +0300

    Add CORS to API

--- base.py
+++ base.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from app import Hello
-from views.user_res import AuthCreate, AuthLogin, MenuOrders, Profile
+from views.user_res import AuthCreate, AuthLogin, Orders
 from views.cater_res import MealMan, Menus, OrdersAll
 from config import config
 
@@ -10,6 +11,8 @@ def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
 
+    CORS(app)
+
     api = Api(app)
 
     from models.models import db
@@ -17,23 +20,22 @@ def create_app(config_filename):
 
     api.add_resource(Hello, '/')
 
-    api.add_resource(AuthCreate, '/api/v2/auth/signup',
-                     '/api/v2/auth/signup/', '/api/v2/users', '/api/v2/users/')
+    api.add_resource(AuthCreate, '/api/v2/auth/signup', '/api/v2/auth/signup/', '/api/v2/users', 
+                     '/api/v2/users/', '/api/v2/auth/make_admin/<int:identity>/', '/api/v2/auth/make_admin/<int:identity>')
 
-    api.add_resource(AuthLogin, '/api/v2/auth/login', '/api/v2/auth/login/')
-
-    api.add_resource(Profile, '/api/v2/profile', '/api/v2/profile/')
+    api.add_resource(AuthLogin, '/api/v2/auth/login', '/api/v2/auth/login/', 
+                     '/api/v2/my_profile/', '/api/v2/my_profile')
 
     api.add_resource(MealMan, '/api/v2/meals/', '/api/v2/meals', '/api/v2/meals/', '/api/v2/meals', '/api/v2/meals/<int:meal_id>',
                      '/api/v2/meals/<int:meal_id>/', '/api/v2/meals/<int:meal_id>', '/api/v2/meals/<int:meal_id>/')
 
-    api.add_resource(Menus, '/api/v2/menu/', '/api/v2/menu')
+    api.add_resource(Menus, '/api/v2/menu/', '/api/v2/menu', '/api/v2/menu/', '/api/v2/menu')
 
-    api.add_resource(OrdersAll, '/api/v2/orders', '/api/v2/orders/')
+    api.add_resource(OrdersAll, '/api/v2/my_orders', '/api/v2/my_orders/')
 
-    api.add_resource(MenuOrders, '/api/v2/menu/', '/api/v2/menu', '/api/v2/orders', '/api/v2/orders/',
-                                 '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/',
-                                 '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/')
+    api.add_resource(Orders, '/api/v2/orders', '/api/v2/orders/', '/api/v2/orders', '/api/v2/orders/', 
+                     '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/', 
+                     '/api/v2/orders/<int:order_id>', '/api/v2/orders/<int:order_id>/')
 
     return app
 
